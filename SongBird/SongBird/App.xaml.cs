1a27ba0b39fa4d100cbf89861ae6aa8e15247fa6
voodhucreative <116579313+voodhucreative@users.noreply.github.com>
2022-10-24 18:54:49 +0100

    first commit

---
+++ SongBird/SongBird/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SongBird.Services;
+using SongBird.Views;
+
+namespace SongBird
+{
+    public partial class App : Application
+    {
+
+        public App ()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
